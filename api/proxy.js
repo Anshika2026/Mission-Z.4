b403dbb476aafc0e43aa8b5f8e83394fa2a813ee
Nauser <nauser@522gmail.com>
2026-03-22 20:14:46 +0530

    'updte00'

--- api/proxy.js
+++ api/proxy.js
@@ -1,15 +1,21 @@
 export default async function handler(req, res) {
   const BASE_URL =
-    "https://script.google.com/macros/s/AKfycbwT85CEvDby69zUFFZ6YHCkyObPwRWZm4RJ_Bj0T151wuG13AMCgL-UOf6m4lrn2lJF/exec";
+    "https://script.google.com/macros/s/AKfycbz1rJ7G6pShVjUP8sdYI-2Jv_Xvvzw9lsvtD74DOcEx_XO7Ou-tk3qZviqNlgRMMG9V/exec";
 
   try {
-    const { action, ...data } = req.method === "POST" ? req.body : req.query;
+    const { action, ...data } = req.body;
+
+    console.log("REQUEST:", action, data);
 
     const params = new URLSearchParams({ action, ...data });
     const url = `${BASE_URL}?${params.toString()}`;
 
+    console.log("FINAL URL:", url);
+
     const response = await fetch(url);
     const text = await response.text();
+
+    console.log("RAW RESPONSE:", text);
 
     let json;
 
@@ -25,6 +31,7 @@ export default async function handler(req, res) {
 
     res.status(200).json(json);
   } catch (error) {
+    console.error("PROXY ERROR:", error);
     res.status(500).json({
       success: false,
       message: error.message,
